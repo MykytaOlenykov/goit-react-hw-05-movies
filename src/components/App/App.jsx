745395f48d355398eb4b2fcdc07f47e0ff62a118
ForestDr <nikita2001395@gmail.com>
2023-04-26 20:48:53 +0300

    add fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,13 +3,15 @@ import Layout from 'components/Layout/Layout';
 import Home from 'pages/Home';
 import Movies from 'pages/Movies';
 import MovieDetails from 'pages/MovieDetails';
+import NotFound from 'components/NotFound/NotFound';
 
 const App = () => (
   <Routes>
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
-      <Route path="/movies" element={<Movies />} />
-      <Route path="/movies/:movieId" element={<MovieDetails />} />
+      <Route path="movies" element={<Movies />} />
+      <Route path="movies/:movieId" element={<MovieDetails />} />
+      <Route path="*" element={<NotFound />} />
     </Route>
   </Routes>
 );
