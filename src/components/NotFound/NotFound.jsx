f4ca4d3fea5debf107002562b14475746cdd485d
ForestDr <nikita2001395@gmail.com>
2023-04-27 22:56:19 +0300

    add notfound component

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,3 +1,10 @@
-const NotFound = () => <div>Not Found</div>;
+import * as S from './NotFound.styled';
+
+const NotFound = () => (
+  <S.Container>
+    <S.ErrorMessage>Not Found</S.ErrorMessage>
+    <S.ErrorCode>404</S.ErrorCode>
+  </S.Container>
+);
 
 export default NotFound;
